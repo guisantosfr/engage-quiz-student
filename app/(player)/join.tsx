41646d8c0eec7558313943ce04635598c33bf9c3
Guilherme Santos <santosgui678@gmail.com>
2026-01-27 16:57:43 -0300

    feat: nativewind config

--- app/(player)/join.tsx
+++ app/(player)/join.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
 
-export default function JoinScreen(){
+export default function JoinScreen() {
     return (
-        <View>
-            <Text>Join Screen</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-xl font-bold text-blue-500">
+                Welcome to Nativewind!
+            </Text>
         </View>
     )
 }
