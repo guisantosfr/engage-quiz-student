0273ced01b2208c624aaa06bc810ac99f439721a
Guilherme Santos <santosgui678@gmail.com>
2026-01-27 17:58:11 -0300

    feat: beginning join screen

--- app/(player)/join.tsx
+++ app/(player)/join.tsx
@@ -1,13 +1,46 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
 import GradientBackground from "@/components/GradientBackground";
+//import Header from "@/components/Header";
 
 export default function JoinScreen() {
     return (
         <GradientBackground>
-            <View className="flex-1 items-center justify-center">
-                <Text className="text-xl font-bold text-white">
-                    Welcome to Nativewind!
+            {/* <Header /> */}
+
+            <View className="flex-1 items-center justify-center gap-5">
+                <Text className="text-3xl font-bold text-white">
+                    EngageQuiz
                 </Text>
+                <Text className="text-xl text-white w-3/4 mx-auto">
+                    Preencha os campos abaixo para se conectar a um questionário
+                </Text>
+
+
+                {/* label */}
+                <Text className="text-md text-white w-3/4 mx-auto">
+                    Apelido / Nickname
+                </Text>
+                <TextInput
+                    placeholder="Como gostaria de ser chamado?"
+                    placeholderTextColor="white"
+                    className="w-3/4 mx-auto p-4 rounded-lg border border-white text-white"
+                    maxLength={20}
+                />
+
+                <Text className="text-md text-white w-3/4 mx-auto">
+                    Código / PIN (6 dígitos)
+                </Text>
+                <TextInput
+                    placeholder="######"
+                    placeholderTextColor="white"
+                    className="w-3/4 mx-auto p-4 rounded-lg border border-white text-white"
+                    maxLength={6}
+                    keyboardType="numeric"
+                />
+
+                <Pressable className="mx-auto p-4 rounded-xl bg-blue-700 w-1/2">
+                    <Text className="text-xl text-white font-bold text-center">Conectar</Text>
+                </Pressable>
             </View>
         </GradientBackground>
     )
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Header() {
+    return (
+        <View className="flex-1">
+            <Text className="text-2xl font-bold">EngageQuiz</Text>
+        </View>
+    );
+}
